8249d07495f467232f4e17f57c2d092f0e57ade3
FebrinaManik <gamerajaa93@gmail.com>
2025-11-02 18:54:53 +0700

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+export default {
+    plugins: {
+        // HAPUS: Hapus plugin tailwindcss karena sudah ditangani oleh vite.config.js
+        // tailwindcss: {}, 
+        autoprefixer: {},
+    },
+};
